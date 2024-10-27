5712ff1b0b6c796773c6be0cc6e09a74fa05b07b
Firzamalik <151676991+firza-malik@users.noreply.github.com>
2024-10-27 08:05:54 -0700

    Add files via upload

---
+++ arrays.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+//array
+	int arrayn[10]={1,3,4,5,6,7,6,8,5,8};
+	cout<<arrayn[4];
+//for each loop	
+	for(int a:arrayn){
+		cout<<a<<endl;
+   }
+//input array
+   int n;
+   cout<<"enter a index num"<<endl;
+   cin>>n;
+   char r[n];
+   cout<<"enter a char";
+   for(int i=0;i<n;i++){
+   	cin>>r[i];
+   	
+   	
+   }
+   cout<<r[];
+	return 0;
+};
